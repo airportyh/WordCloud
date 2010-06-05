37c5299d731f1ad30b85bbd0cd8d51aa88dcdfdc
airportyh <airportyh@gmail.com>
2010-06-05 19:23:13 -0400

    fixed common word filtering and build script

--- wordyclouds.js
+++ wordyclouds.js
@@ -1,24 +1,24 @@
 var CommonWords = {}
 CommonWords.english = "a about after again against all an another any and are as at\
-be being been before but by\
-can could\
-did do don't down\
-each\
-few from for\
-get got great\
-had has have he her here his him himself hers how\
-i if i'm in into is it it's\
-just\
-like\
-made me more most my\
-no not\
-of off on once one only or other our out over own\
-said she should so some such\
-than that the their them then there these they this those through to too\
-under until up\
-very\
-was wasn't we were we're what when where which while who why will with would wouldn't\
-you your".split(' ')
+ be being been before but by\
+ can could\
+ did do don't down\
+ each\
+ few from for\
+ get got great\
+ had has have he her here his him himself hers how\
+ i if i'm in into is it it's\
+ just\
+ like\
+ made me more most my\
+ no not\
+ of off on once one only or other our out over own\
+ said she should so some such\
+ than that the their them then there these they this those through to too\
+ under until up\
+ very\
+ was wasn't we were we're what when where which while who why will with would wouldn't\
+ you your".split(' ')
 
 var ColorPalates = {}
 ColorPalates.autumn = 
@@ -58,7 +58,7 @@ function tokenize(text, commonWords){
             return word.toLowerCase()
         })
         .filter(function(word){
-        	return !commonWords || commonWords.indexOf(word) == -1
+        	return commonWords.indexOf(word) == -1
         })
 }
 
